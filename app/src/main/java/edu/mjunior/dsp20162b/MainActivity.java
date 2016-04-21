c2edd299fbe44d3c296df6b62c751fc5b192ec00
Junior Michalski <Junior Michalski>
2016-04-21 15:39:49 -0300

    Android Annotations

---
+++ app/src/main/java/edu/mjunior/dsp20162b/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.mjunior.dsp20162b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @AfterViews
+    public void inicio () {
+
+    }
+}
